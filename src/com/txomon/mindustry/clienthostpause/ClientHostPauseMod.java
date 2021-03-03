4afab65d53f67f25188f80a59544dd79fed68fef
Javier Domingo Cansino <javierdo1@gmail.com>
2021-03-04 00:30:43 +0100

    Doing suggested solution of serverPaused instead of patching GameState

--- src/com/txomon/mindustry/clienthostpause/ClientHostPauseMod.java
+++ src/com/txomon/mindustry/clienthostpause/ClientHostPauseMod.java
@@ -1,21 +1,24 @@
 package com.txomon.mindustry.clienthostpause;
 
+import arc.Core;
 import arc.Events;
-import arc.util.Log;
 import mindustry.Vars;
+import mindustry.core.GameState;
 import mindustry.game.EventType;
+import mindustry.input.Binding;
 import mindustry.mod.Mod;
+
+import static arc.Core.scene;
+import static mindustry.Vars.state;
+import static mindustry.Vars.ui;
 
 public class ClientHostPauseMod extends Mod {
     @Override
     public void init() {
-        Events.run(EventType.ResetEvent.class, this::patchGameState);
-    }
-    public void patchGameState() {
-        if (!(Vars.state instanceof PausableGameState)) {
-            PausableGameState newState = PausableGameState.copyFrom(Vars.state);
-            Log.info("Trying to replace object @ with @", Vars.state, newState);
-            Vars.state = newState;
-        }
+        Events.run(EventType.Trigger.update, () -> {
+            if (Core.input.keyTap(Binding.pause) && !scene.hasDialog() && !scene.hasKeyboard() && !ui.restart.isShown() && (state.is(GameState.State.paused) || state.is(GameState.State.playing))) {
+                Vars.state.serverPaused = state.is(GameState.State.playing);
+            }
+        });
     }
 }
