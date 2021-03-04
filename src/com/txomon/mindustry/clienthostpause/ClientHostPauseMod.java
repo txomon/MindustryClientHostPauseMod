2995e0732d4c7a28e9f5cb652b97e42fc106a34f
Javier Domingo Cansino <javierdo1@gmail.com>
2021-03-04 21:18:55 +0100

    Fix server->client serverPaused propagation on manual change

--- src/com/txomon/mindustry/clienthostpause/ClientHostPauseMod.java
+++ src/com/txomon/mindustry/clienthostpause/ClientHostPauseMod.java
@@ -1,23 +1,20 @@
 package com.txomon.mindustry.clienthostpause;
 
-import arc.Core;
-import arc.Events;
-import arc.util.Time;
-import arc.util.Log;
-import mindustry.Vars;
-import mindustry.core.GameState;
-import mindustry.game.EventType;
-import mindustry.gen.Groups;
-import mindustry.gen.Player;
-import mindustry.input.Binding;
-import mindustry.mod.Mod;
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.core.*;
+import mindustry.game.*;
+import mindustry.gen.*;
+import mindustry.input.*;
+import mindustry.mod.*;
+
+import java.io.*;
 
 import static arc.Core.scene;
-import static mindustry.Vars.state;
-import static mindustry.Vars.ui;
+import static mindustry.Vars.*;
 
 public class ClientHostPauseMod extends Mod {
-    private long lastForceSync = 0L;
     @Override
     public void init() {
         Events.run(EventType.Trigger.update, () -> {
@@ -27,33 +24,20 @@ public class ClientHostPauseMod extends Mod {
             }
 
             // If statement copied from https://github.com/Anuken/Mindustry/blob/d73cf9fcff026a581b06c0aaab89a9c3231d3310/core/src/mindustry/core/Control.java#L540
-            if (Core.input.keyTap(Binding.pause) && !scene.hasDialog() && !scene.hasKeyboard() && !ui.restart.isShown() && (state.is(GameState.State.paused) || state.is(GameState.State.playing))) {
+            if (Core.input.keyTap(Binding.pause) && !scene.hasDialog() && !scene.hasKeyboard() && !ui.restart.isShown() && (state.is(GameState.State.paused) || state.is(GameState.State.playing))){
                 // Boolean flip
                 state.serverPaused = !state.serverPaused;
-            }
-
-            // If server is playing, no need to push changes, as sendWorldData() will reset the view
-            if (!state.serverPaused) {
-                return;
-            }
-
-            // Don't update always
-            if (Time.timeSinceMillis(lastForceSync) < 2000L) {
-                return;
-            }
-            lastForceSync = Time.millis();
-
-            Groups.player.update();
-            for (Player player : Groups.player){
-                if (player.isLocal()) {
-                    continue;
+                // Because GameState updates are not sent through (not even to the host), call the smallest function that replicates GameState
+                for(Player player : Groups.player){
+                    if(player.isLocal()){
+                        continue;
+                    }
+                    try{
+                        netServer.writeEntitySnapshot(player);
+                    }catch(IOException e){
+                        Log.err("Failed to notify player @ of server pause/resume", player);
+                    }
                 }
-                try {
-                    player.getInfo().lastSyncTime = Time.millis();
-                } catch (IllegalArgumentException e) {
-                    Log.err("Programming error: Failed to set lastSyncTime of player @ on pause/resume", player);
-                }
-                Vars.netServer.sendWorldData(player);
             }
         });
     }
